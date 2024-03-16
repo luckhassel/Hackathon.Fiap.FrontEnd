d2e84d6394a0ac4996cd3709142d4f8dcc387f14
Guilherme <guilhermeluckfett@gmail.com>
2024-03-16 14:53:52 -0300

    fix: api endpoints

--- Hackathon.Fiap.Frontend/src/app/Constants/Http/video.constants.ts
+++ Hackathon.Fiap.Frontend/src/app/Constants/Http/video.constants.ts
@@ -2,6 +2,6 @@ export const INTERNAL_DOMAIN = '';
 export const BLOB_DOMAIN = 'https://testandoluck.blob.core.windows.net/test';
 
 export const VIDEO_API_ENDPOINTS = {
-    List: './assets/list.mock.json',
-    Create: ''
+    List: '/uploads',
+    Create: '/upload:start'
 };
